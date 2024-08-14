6c5ceede04610ef39892451f9c83bd76abd121b0
DorantVelija <dorantvelija@outlook.com>
2024-08-14 21:46:01 +0200

    update footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Heading from './components/Heading';
+import Button from './components/Button';
+import Paragraph from './components/Paragraph';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+        <Nav />
+        <Heading color="black" size="h1" weight="bold">Hello World!</Heading>
+        <Button color="primary" size="small">Click Me</Button>
+        <Paragraph width="full" size="normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat blanditiis numquam hic placeat, nisi ut mollitia quis commodi repellendus, iure quae magnam consequatur facere saepe dolorum, amet praesentium cupiditate nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat blanditiis numquam hic placeat, nisi ut mollitia quis commodi repellendus, iure quae magnam</Paragraph>
+        <Footer />
     </div>
   );
 }
