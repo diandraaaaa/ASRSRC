ebdd1bda47db27141506ab27ee91155130d530ee
DorantVelija <dorantvelija@outlook.com>
2024-08-16 00:11:48 +0200

    fix dropdown so you're able to select text

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -12,12 +12,13 @@ function Dropdown({title, text}) {
     };
 
   return (
-    <div className="mx-4 md:mx-32 hover:bg-gray-50 rounded-tl-[10px] rounded-tr-[10px] py-6 border-b-[3px] border-[#27C25C]" onClick={dropdownToggle}>
-        <div className="flex items-center justify-between md:px-16">
+    <div className="mx-4 md:mx-32 hover:bg-gray-50 rounded-tl-[10px] rounded-tr-[10px] py-6 border-b-[3px] border-[#27C25C]" >
+        <div className="flex items-center justify-between md:px-16 cursor-pointer" onClick={dropdownToggle}>
          <Heading color="black" size="h3" weight="bold">{title}</Heading>
          <img 
             src={IconUp}
             className={`max-md:size-12 transition-transform duration-300 ${isDropdownVisible ? 'rotate-180' : ''}`} 
+            
         />
         </div>   
         { isDropdownVisible ? (<div><Paragraph color="black" size="normal" customClasses="m-4 px-2 md:px-16">{text}</Paragraph></div>) : ' '}
