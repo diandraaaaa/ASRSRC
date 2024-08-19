dd52d2e4a5a59c32967453f55ef8d6249577ad79
diandraaaaa <diandra.stoica@stud.ubbcluj.ro>
2024-08-19 20:04:17 +0300

    made the text nice for the preview lol

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,7 +44,23 @@ function Home() {
         <div className="max-w-5xl mx-auto">
           <img src={Image2} alt="" className="mx-auto max-md:px-4 md:max-w-md xl:max-w-6xl mb-12"/>
           <Heading size="h4" underline="true" weight="bold" align="center"> Antreprenoriat social rural sustenabil in Regiunea Centru</Heading>
-          <Paragraph customClasses="my-14 px-12" underline="true">{continut}
+          <Paragraph customClasses="my-14 px-12" underline="true">📌Ai o idee de afacere și nu știi de unde să începi? <br/>
+          📌Ai un plan de afaceri, dar nu ai resurse financiare pentru a-l pune în aplicare?<br/><br/>
+
+          Înscrie-te în proiectul „Antreprenoriat social urban sustenabil în Regiunea Centru” și descoperă cum poți să-ți transformi visul în realitate. 🌳🌳<br/><br/>
+
+          Cine poate participa?<br/>
+          ✅ Persoane cu domiciliul în mediul rural din regiunea Centru<br/>
+          ✅ Persoane cu vârsta 18-64 ani<br/>
+          ✅ Studii medii finalizate <br/><br/>
+
+          Vei beneficia de <br/>
+          -	Cursuri GRATUITE de specializare în Antreprenoriat social și de o subvenție de participare de 900 lei<br/>
+          -	Mentorat și consiliere în domeniul antreprenoriatului social<br/>
+          -	Șansa de a câștiga un grant de pana la 100.000 euro pentru finanțarea afacerii tale.<br/><br/>
+
+          🎯Înscrie-te acum și ai posibilitatea de a-ți transforma visul în realitate. <br/>
+          🎯Contribuie și tu la dezvoltarea comunității tale.<br/>
           <br/><br/>
           Aceasta este pagina de prezentare a proiectului european cu titlul – „Antreprenoriat social urban sustenabil în Regiunea Centru”, proiect cofinanțat din Fondul Social European prin Programul Incluziune si Demnitate Sociala 2021-2027, ce este derulat de către Asociația Institutul Educațional pentru Politici Sociale Margareta în parteneriat cu Fundația Europeană pentru Consultanță, Implementare și Dezvoltare și Fundația Progpers, în perioada 01 iulie 2024 – 31 decembrie 2026.
           <br/><br/>
