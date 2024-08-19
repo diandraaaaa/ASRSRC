b584b7bb7aacc138b100f7069166e55c69e9b723
diandraaaaa <diandra.stoica@stud.ubbcluj.ro>
2024-08-19 13:19:09 +0300

    added the content not the best form tho lmao and left a comment wih what i thhink needs to be done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,13 +11,31 @@ import Image from '../assets/landing-img.png'
 import Image2 from '../assets/home-img.png'
 
 function Home() {
+  const continut = `📌Ai o idee de afacere și nu știi de unde să începi? <br/>
+          📌Ai un plan de afaceri, dar nu ai resurse financiare pentru a-l pune în aplicare?<br/><br/>
+
+          Înscrie-te în proiectul „Antreprenoriat social urban sustenabil în Regiunea Centru” și descoperă cum poți să-ți transformi visul în realitate. 🌳🌳<br/><br/>
+
+          Cine poate participa?<br/>
+          ✅ Persoane cu domiciliul în mediul rural din regiunea Centru<br/>
+          ✅ Persoane cu vârsta 18-64 ani<br/>
+          ✅ Studii medii finalizate <br/><br/>
+
+          Vei beneficia de <br/>
+          -	Cursuri GRATUITE de specializare în Antreprenoriat social și de o subvenție de participare de 900 lei<br/>
+          -	Mentorat și consiliere în domeniul antreprenoriatului social<br/>
+          -	Șansa de a câștiga un grant de pana la 100.000 euro pentru finanțarea afacerii tale.<br/><br/>
+
+          🎯Înscrie-te acum și ai posibilitatea de a-ți transforma visul în realitate. <br/>
+          🎯Contribuie și tu la dezvoltarea comunității tale.<br/>`
 
   return (
     <>
+    {/* TO-DO i think the text should be moved a lil up and make the button do smth also the link at the end should be green and underliend*/}
       <section className="text-center py-32 h-screen">
         <div>
           <img src={Image} alt="" className="absolute max-md:hidden max-w-xl z-0"/>
-          <Heading color="black" size="h1" weight="bold" customClasses="md:mt-14 max-w-5xl mx-auto z-9">“Empowering Tomorrow, One <span className="text-[#27C25C]">Green</span> Choice at a Time.”</Heading>
+          <Heading color="black" size="h1" weight="bold" customClasses="md:mt-14 max-w-5xl mx-auto z-9">Înscrie-te în proiectul <span className="text-[#27C25C]">„Antreprenoriat social urban sustenabil în Regiunea Centru” </span> și descoperă cum poți să-ți transformi visul în realitate.n</Heading>
           <Button color="secondary" size="small" customClasses="block mx-auto z-9">Citeste mai mult</Button>
         </div>
       </section>
@@ -26,9 +44,12 @@ function Home() {
         <div className="max-w-5xl mx-auto">
           <img src={Image2} alt="" className="mx-auto max-md:px-4 md:max-w-md xl:max-w-6xl mb-12"/>
           <Heading size="h4" underline="true" weight="bold" align="center"> Antreprenoriat social rural sustenabil in Regiunea Centru</Heading>
-          <Paragraph customClasses="my-14 px-12" underline="true">Aceasta este pagina de prezentare a proiectului european cu titlul – „Antreprenoriat social urban sustenabil în Regiunea Centru”, proiect cofinanțat din Fondul Social European prin Programul Incluziune si Demnitate Sociala 2021-2027, ce este derulat de către Asociația Institutul Educațional pentru Politici Sociale Margareta în parteneriat cu Fundația Europeană pentru Consultanță, Implementare și Dezvoltare și Fundația Progpers, în perioada 01 iulie 2024 – 31 decembrie 2026.
+          <Paragraph customClasses="my-14 px-12" underline="true">{continut}
           <br/><br/>
-            Pentru informații detaliate despre celelalte programe cofinanțate de Uniunea Europeană, vă invităm să vizitați mfe.gov.ro</Paragraph>
+          Aceasta este pagina de prezentare a proiectului european cu titlul – „Antreprenoriat social urban sustenabil în Regiunea Centru”, proiect cofinanțat din Fondul Social European prin Programul Incluziune si Demnitate Sociala 2021-2027, ce este derulat de către Asociația Institutul Educațional pentru Politici Sociale Margareta în parteneriat cu Fundația Europeană pentru Consultanță, Implementare și Dezvoltare și Fundația Progpers, în perioada 01 iulie 2024 – 31 decembrie 2026.
+          <br/><br/>
+            Pentru informații detaliate despre celelalte programe cofinanțate de Uniunea Europeană, vă invităm să vizitați <a href='https://mfe.gov.ro'>mfe.gov.ro</a>
+          </Paragraph>
         </div>
       </section>
 
